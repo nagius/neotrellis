72f09e0db3f87adb25143aa54a446e7328a54843
Nicolas AGIUS <nicolas.agius@lps-it.fr>
2019-09-28 19:01:33 +0100

    Refactor read_raw()

--- lib/neotrellis/seesaw.rb
+++ lib/neotrellis/seesaw.rb
@@ -1,13 +1,5 @@
 
 # TODO setup logger
-# TODO debug flag
-
-# TODO doc avec event et sans event
-
-# TODO verifier assert boolean
-
-# Check function a passer en private 
-# TODO read_raw en private
 
 require 'i2c'
 
@@ -25,13 +17,6 @@ module Neotrellis
 	class SeeSaw
 		# Default SeeSaw I2C address is 0x49
 		DEFAULT_I2C_ADDR = 0x49
-		HW_ID_CODE = 0x55
-
-		STATUS_BASE = 0x00
-		STATUS_SWRST = 0x7F
-		STATUS_HW_ID = 0x01
-		STATUS_VERSION = 0x02
-
 		def initialize(device: '/dev/i2c-0', addr: DEFAULT_I2C_ADDR, debug: false)
 			@i2c = I2C.create(device)
 			@addr = addr
@@ -62,27 +47,37 @@ module Neotrellis
 			@i2c.read(@addr, 4, STATUS_BASE, STATUS_VERSION).unpack('I>').first
 		end
 
-	# TODO refactor with read_raw
-	# TODO add debug message
 		def read_byte(base_reg, function_reg)
-			@i2c.read(@addr, 1, base_reg, function_reg).ord
+			read_raw(1, base_reg, function_reg).ord
 		end
 
 		def read_bytes(size, base_reg, function_reg)
-			@i2c.read(@addr, size, base_reg, function_reg).unpack("C#{size}")
+			read_raw(size, base_reg, function_reg).unpack("C#{size}")
 		end
 
-		def read_raw(len, base_reg, function_reg)
-			@i2c.read(@addr, len, base_reg, function_reg)
-		end
-
+		# TODO doc data must be byte array or string big endian
 		def write(base_reg, function_reg, *data)
-			# TODO doc data must be byte array or string big endian
 			puts "DEBUG: I2C WRITE: %02X %02X %s" % [base_reg, function_reg, data.map{|i| "%02X" % [i]}.join(' ')] if @debug
 			@i2c.write(@addr, base_reg, function_reg, *data)
 		end
 
 		private
+
+		# SeeSaw hardware ID
+		HW_ID_CODE = 0x55
+
+		# Internal SeeSaw registers
+		STATUS_BASE = 0x00
+		STATUS_SWRST = 0x7F
+		STATUS_HW_ID = 0x01
+		STATUS_VERSION = 0x02
+
+		def read_raw(size, base_reg, function_reg)
+			data = @i2c.read(@addr, size, base_reg, function_reg)
+			puts "DEBUG: I2C READ: %02X %02X %s" % [base_reg, function_reg, data.unpack("C#{size}").map{|i| "%02X" % [i]}.join(' ')] if @debug
+
+			data
+		end
 
 		def testing?
 			ENV['RSPEC_TEST']&.downcase == 'true'
